5553c594a61f19f3fe9c5e104395fedf5bd3dd18
matias <pagosptcmatias@gmail.com>
2022-11-04 16:44:43 -0300

    limpie un poco el codigo y comente la opcion exa en models

--- src/models/colores.js
+++ src/models/colores.js
@@ -7,12 +7,12 @@ const colorSchema = new Schema({
     minLength: 2,
     maxLength: 500,
   },
-  codHexa: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 10000,
-  },
+  // codHexa: {
+  //   type: String,
+  //   required: false,
+  //   minLength: 1,
+  //   maxLength: 10000,
+  // },
 });
 
 const Color = mongoose.model("color", colorSchema);
